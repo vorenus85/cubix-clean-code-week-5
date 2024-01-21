a7cd86105d0d367d3afe021073511d4dfd8b5622
Janos Perge <janos.perge@gmail.com>
2024-01-21 22:41:46 +0100

    replace switch statement at calculateDiscountPercentage

--- src/calculateDiscountPercentage.ts
+++ src/calculateDiscountPercentage.ts
@@ -1,22 +1,17 @@
 export function calculateDiscountPercentage(level: string): number {
-  let discountPercentage: number
-  switch (level) {
-    case 'standard':
-      discountPercentage = 5
-      break
-    case 'silver':
-      discountPercentage = 10
-      break
-    case 'gold':
-      discountPercentage = 15
-      break
-    default:
-      discountPercentage = 0
-      break
+  let defaultPercentage = 0
+  let discountPercentage = defaultPercentage
+
+  const discountByLevel = {
+    standard: 5,
+    silver: 10,
+    gold: 20,
   }
-  if (level === 'gold') {
-    // Additional discount for gold members
-    discountPercentage += 5
+
+  if (discountByLevel.hasOwnProperty(level)) {
+    discountPercentage = discountByLevel[level as keyof typeof discountByLevel]
+    return discountPercentage
+  } else {
+    throw new Error('Invalid discount level, discount level not defined')
   }
-  return discountPercentage
 }
--- test/calculateDiscountPercentage.test.ts
+++ test/calculateDiscountPercentage.test.ts
@@ -1,22 +1,45 @@
 import { calculateDiscountPercentage } from '../src/calculateDiscountPercentage'
 
 describe('calculate discount percentage', () => {
-  test.each`
-    discountLevel | percentage
-    ${'standard'} | ${5}
-    ${'silver'}   | ${10}
-    ${'gold'}     | ${20}
-    ${''}         | ${0}
-    ${'custom'}   | ${0}
-    ${'asd'}      | ${0}
-  `(
-    '$discountLevel discount level should give $percentage percent discount',
-    ({ discountLevel, percentage }) => {
-      // Act
-      const actualResult = calculateDiscountPercentage(discountLevel)
+  describe('Happy path', () => {
+    test.each`
+      discountLevel | percentage
+      ${'standard'} | ${5}
+      ${'silver'}   | ${10}
+      ${'gold'}     | ${20}
+    `(
+      '$discountLevel discount level should give $percentage percent discount',
+      ({ discountLevel, percentage }) => {
+        // Act
+        const actualResult = calculateDiscountPercentage(discountLevel)
 
-      // Assert
-      expect(actualResult).toBe(percentage)
-    }
-  )
+        // Assert
+        expect(actualResult).toBe(percentage)
+      }
+    )
+  })
+
+  describe('Error path', () => {
+    test.each`
+      discountLevel
+      ${'custom'}
+      ${'default'}
+      ${'asd'}
+      ${''}
+    `(
+      'at $discountLevel discount level should throw error because discount level not exist',
+      ({ discountLevel }) => {
+        // Act
+        const errorMessage =
+          'Invalid discount level, discount level not defined'
+        const errorExpected = new Error(errorMessage)
+
+        // Assert
+        // Act and Assert
+        expect(() => calculateDiscountPercentage(discountLevel)).toThrow(
+          errorExpected
+        )
+      }
+    )
+  })
 })
