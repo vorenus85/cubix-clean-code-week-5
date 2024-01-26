a0fa3ff125fa9216ad0f97629b2577ad78fcc07a
Janos Perge <janos.perge@gmail.com>
2024-01-26 20:12:00 +0100

    extend jest config, extend package.json scripts

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,19 @@
 module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    testMatch: ['<rootDir>/test/**/*.test.ts'],
-    moduleFileExtensions: ['ts', 'js', 'json', 'node'],
-    collectCoverage: true,
-    coverageDirectory: "coverage",
-    coverageProvider: "v8",
-  };
-  
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['<rootDir>/test/**/*.test.ts'],
+  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coveragePathIgnorePatterns: ['/I.*\\.ts$'],
+  coverageProvider: 'v8',
+  collectCoverageFrom: ['./src/**'],
+  coverageThreshold: {
+    global: {
+      statements: 90,
+      branches: 90,
+      functions: 90,
+      lines: 90,
+    },
+  },
+}
