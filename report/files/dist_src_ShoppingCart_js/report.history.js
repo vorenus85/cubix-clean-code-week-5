9242022d505b82fa531bcc7cab84f3162cbcb92a
Janos Perge <janos.perge@gmail.com>
2024-01-27 00:32:14 +0100

    add plato report

---
+++ report/files/dist_src_ShoppingCart_js/report.history.js
@@ -0,0 +1,1 @@
+__history = [{"date":"Fri, 26 Jan 2024 23:30:28 GMT","sloc":26,"lloc":17,"functions":4,"deliveredBugs":0.149,"difficulty":16.667,"maintainability":82.746,"lintErrors":12}]
---
+++ report/files/dist_src_calculateDiscountPercentage_js/report.history.js
@@ -0,0 +1,1 @@
+__history = [{"date":"Fri, 26 Jan 2024 23:30:28 GMT","sloc":20,"lloc":17,"functions":1,"deliveredBugs":0.11,"difficulty":11.136,"maintainability":62.322,"lintErrors":7}]
